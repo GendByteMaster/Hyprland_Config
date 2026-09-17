d4bfe259b90fc2a08999d800bb541aa45f32f3ee
Gend <GendByteMaster@gmail.com>
2026-09-17 11:37:14 +0300

    feat: connect workstation bindings to Omarchy

---
+++ hypr/bindings.lua
@@ -0,0 +1,10 @@
+local home = os.getenv("HOME") or ""
+local preserved_bindings = home .. "/.local/state/hyprland_config/preserved_bindings.lua"
+
+local preserved = io.open(preserved_bindings, "r")
+if preserved then
+  preserved:close()
+  dofile(preserved_bindings)
+end
+
+require("hypr.workstation.mouse").register(hl, o)
