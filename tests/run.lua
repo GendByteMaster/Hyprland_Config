712cf2a45a49ade31175d77383721d6d3a568a41
Gend <GendByteMaster@gmail.com>
2026-09-17 11:58:32 +0300

    test: include Num Lock store suite

--- tests/run.lua
+++ tests/run.lua
@@ -1,6 +1,7 @@
 package.path = "./?.lua;./?/init.lua;./lua/?.lua;./lua/?/init.lua;" .. package.path
 
 require("tests.mouse_state_test")
+require("tests.numlock_store_test")
 require("tests.mouse_test")
 require("tests.installer_test")
 require("tests.uninstaller_test")
