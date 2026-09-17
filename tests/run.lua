6dd3223be9adfb4df9a4912b773974445f2a004f
Gend <GendByteMaster@gmail.com>
2026-09-17 11:38:12 +0300

    test: include uninstaller suite

--- tests/run.lua
+++ tests/run.lua
@@ -3,5 +3,6 @@ package.path = "./?.lua;./?/init.lua;./lua/?.lua;./lua/?/init.lua;" .. package.p
 require("tests.mouse_state_test")
 require("tests.mouse_test")
 require("tests.installer_test")
+require("tests.uninstaller_test")
 
 require("tests.testlib").run()
