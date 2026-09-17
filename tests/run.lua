ceda82fddc5744a7ce312ecb813904a19d42810a
Gend <GendByteMaster@gmail.com>
2026-09-17 22:08:54 +0300

    test(telemetry): run collector tests

--- tests/run.lua
+++ tests/run.lua
@@ -12,5 +12,6 @@ require("tests.installer_test")
 require("tests.uninstaller_test")
 require("tests.verifier_test")
 require("tests.telemetry_test")
+require("tests.telemetry_collector_test")
 
 require("tests.testlib").run()
