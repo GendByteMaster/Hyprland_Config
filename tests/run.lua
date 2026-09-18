d7ce6943bd40350a1b41e698ea0f2d03cbe1b5ab
Gend <GendByteMaster@gmail.com>
2026-09-18 11:42:31 +0300

    test: register RED project action tests

--- tests/run.lua
+++ tests/run.lua
@@ -24,5 +24,7 @@ require("tests.project_discovery_test")
 require("tests.project_state_test")
 require("tests.project_search_test")
 require("tests.project_cache_test")
+require("tests.project_types_test")
+require("tests.project_actions_test")
 
 require("tests.testlib").run()
