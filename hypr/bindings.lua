a63e691894ff805d10e7b0e4dd638d0ca6eb653c
Gend <GendByteMaster@gmail.com>
2026-09-19 00:29:37 +0300

    feat: register workspace overview binding

--- hypr/bindings.lua
+++ hypr/bindings.lua
@@ -10,3 +10,4 @@ end
 require("hypr.workstation.compat").apply(hl)
 require("hypr.workstation.mouse").register(hl, o)
 require("hypr.workstation.project_launcher").register(hl, o)
+require("hypr.workstation.workspace_overview").register(hl, o)
