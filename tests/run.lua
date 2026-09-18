e77bc06926cc192c1df390c690cc5ebd4eb402c6
Gend <GendByteMaster@gmail.com>
2026-09-19 00:12:24 +0300

    test: include workspace overview QML suite

--- tests/run.lua
+++ tests/run.lua
@@ -33,6 +33,7 @@ require("tests.action_executor_test")
 require("tests.launcher_protocol_test")
 require("tests.project_launcher_cli_test")
 require("tests.project_launcher_qml_test")
+require("tests.workspace_overview_qml_test")
 require("tests.project_launcher_binding_test")
 require("tests.launcher_installer_test")
 
