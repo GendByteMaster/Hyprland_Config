96863ad385926f03a8e2e7945913aa33992bd87c
Gend <GendByteMaster@gmail.com>
2026-09-20 20:49:23 +0300

    refactor: keep Windows shortcut layer out of overview branch

--- hypr/bindings.lua
+++ hypr/bindings.lua
@@ -10,5 +10,4 @@ end
 require("hypr.workstation.compat").apply(hl)
 require("hypr.workstation.mouse").register(hl, o)
 require("hypr.workstation.project_launcher").register(hl, o)
-require("hypr.workstation.windows_shortcuts").register(hl, o)
 require("hypr.workstation.workspace_overview").register(hl, o)
