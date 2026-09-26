a6e1446789bb317c2c36906027ef20fd7768e5ff
Gend <GendByteMaster@gmail.com>
2026-09-26 10:06:50 +0300

    test(spatial): expect Super Alt G toggle

--- tests/spatial_binding_test.lua
+++ tests/spatial_binding_test.lua
@@ -150,7 +150,7 @@ t.test("spatial bindings own only their explicit chords when plugin is available
   t.eq(#calls.unbinds, 6)
   t.eq(#calls.binds, 10)
 
-  t.eq(calls.binds[1].keys, "CTRL + SUPER + G")
+  t.eq(calls.binds[1].keys, "SUPER + ALT + G")
   t.eq(calls.binds[2].keys, "SUPER + ALT + LEFT")
   t.eq(calls.binds[3].keys, "SUPER + ALT + LEFT")
   t.eq(calls.binds[4].keys, "SUPER + ALT + RIGHT")
